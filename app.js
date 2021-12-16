5e633590d2c240ae8b4b3efaf84cbe407fba6fd6
ZavadskiyAS <Zavadskiyweb@gmail.com>
2021-12-16 16:34:30 +0200

    final

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const startBtn = document.querySelector('#start');
 const screens = document.querySelectorAll('.screen');
 const timeList = document.querySelector('#time-list');
-const timeEl = document.querySelector('#time')
+const timeEl = document.querySelector('#time');
+const board = document.querySelector('#board')
 
-let time = 20;
+let time = 0;
+let score = 0;  
 startBtn.addEventListener('click', (event)=>{
     event.preventDefault()
     screens[0].classList.add('up')
@@ -11,23 +13,63 @@ startBtn.addEventListener('click', (event)=>{
 
 timeList.addEventListener('click', event => {
     if (event.target.classList.contains('time-btn') ) {
-        time = parseInt(event.target.getAttributte('data-time'))
+        time = parseInt(event.target.getAttribute('data-time'))
         screens[1].classList.add('up')
         startGame()
     }
 })
-startGame()
+
+board.addEventListener('click', event =>{
+    if (event.target.classList.contains('circle')) {
+        score++
+        event.target.remove()
+        createRandomCircle()
+    }
+})
 
 function startGame() {
     setInterval(decreaseTime, 1000)    
-    timeEl.innerHTML = `00:${time}`
+    createRandomCircle()
+    setGame(time)
 }
 
 function decreaseTime() {
-    let current = --time;
-    if (current < 10) {
-        current = `0${current}`
+    if (time === 0) {
+        finishGame()
+    } else {
+        let current = --time;
+        if (current < 10) {
+            current = `0${current}`
+        }
+        setTime(current)
     }
-    timeEl.innerHTML = `00:${current}`
-
 }
+
+function setTime(value){
+    timeEl.innerHTML = `00:${value}`
+}
+
+function finishGame() {
+    timeEl.parentNode.classList.add('hide')
+    board.innerHTML = `<h1>Счет:<span class="primary">${score}</span></h1>`
+}
+
+function createRandomCircle() {
+    const circle = document.createElement('div')
+    const size = getRandomNumber(10, 60)
+    const {width, height} = board.getBoundingClientRect()
+    const x = getRandomNumber(0, width - size)
+    const y = getRandomNumber(0, height - size)
+    circle.classList.add('circle')
+    circle.style.width = `${size}px`
+    circle.style.height = `${size}px`
+    circle.style.top = `${y}px`
+    circle.style.left = `${x}px`
+
+    board.append(circle)
+}
+
+function getRandomNumber(min, max) {
+    return Math.round(Math.random() * (max - min) + min)
+}
+
